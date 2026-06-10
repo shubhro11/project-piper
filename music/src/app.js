c755e463235ee919f09de89eec546769f434a7ee
shubhro11 <shubhro.seby11@gmail.com>
2026-06-10 13:16:44 +0530

    feat: set up Express app with middleware and add music routes

--- music/src/app.js
+++ music/src/app.js
@@ -1,6 +1,18 @@
 import express from "express"
+import cookieParser from "cookie-parser"
+import morgan from "morgan"
+
+// Import Routes
+import musicRoutes from "./routes/music.route.js"
 
 const app = express()
 
+app.use(express.json())
+app.use(cookieParser())
+app.use(morgan("dev"))
+
+
+// Routes
+app.use("/api/music", musicRoutes)
 
 export default app
---
+++ music/src/models/music.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const musicSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  artist: {
+    type: String,
+    required: true,
+  },
+  artistId: {
+    type: mongoose.Schema.Types.ObjectId,
+  },
+  musicUrl: {
+    type: String,
+    required: true,
+  },
+  coverImageUrl: {
+    type: String,
+    required: true,
+  },
+}, { timestamps: true});
+
+const musicModel = mongoose.model("music", musicSchema);
+
+export default musicModel;
