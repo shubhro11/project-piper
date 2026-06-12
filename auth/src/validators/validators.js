e9e7ff15e10519f6824bd05fe77d4fc4039c94e9
shubhro11 <shubhro.seby11@gmail.com>
2026-06-12 19:38:48 +0530

    feat(validators): enhance error response structure and refine password validation rules

--- auth/src/validators/validators.js
+++ auth/src/validators/validators.js
@@ -4,7 +4,7 @@ const validateErrorResponse = (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty())
-    return res.status(400).json({ errors: errors.array() });
+    return res.status(400).json({ success: false, errors: errors.array() });
   next();
 };
 
@@ -76,17 +76,21 @@ export const registerUserRules = [
     .withMessage("Password is required")
     .isLength({ min: 8 })
     .withMessage("Password must be at least 8 characters long")
-    .matches(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&^#()[\]{}\-_=+|\\/.,:;"'<>~`]).+$/,
-    )
-    .withMessage(
-      "Password must contain at least 1 uppercase letter, 1 lowercase letter, 1 number, and 1 special character",
-    ),
+    .matches(/[A-Z]/)
+    .withMessage("Password must contain at least 1 uppercase letter")
+    .matches(/[a-z]/)
+    .withMessage("Password must contain at least 1 lowercase letter")
+    .matches(/\d/)
+    .withMessage("Password must contain at least 1 number")
+    .matches(/[@$!%*?&^#()[\]{}\-_=+|\\/.,:;"'<>~`]/)
+    .withMessage("Password must contain at least 1 special character"),
 
   body("role")
     .optional()
     .isIn(["user", "artist"])
     .withMessage("Role must be either 'user' or 'artist'"),
+
+  validateErrorResponse,
 ];
 
 export const loginUserRules = [
@@ -142,4 +146,6 @@ export const loginUserRules = [
     .withMessage(
       "Password must contain at least 1 uppercase letter, 1 lowercase letter, 1 number, and 1 special character",
     ),
+
+  validateErrorResponse,
 ];
